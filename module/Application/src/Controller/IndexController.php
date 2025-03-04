7951b62ebab14fee5d62be23e29967be282ebd4b
Zach <zlee@nucleussec.com>
2025-03-04 07:25:04 -0500

    set up the boilerplate with initial changes

---
+++ module/Application/src/Controller/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Controller;
+
+use Laminas\Mvc\Controller\AbstractActionController;
+use Laminas\View\Model\ViewModel;
+
+class IndexController extends AbstractActionController
+{
+    public function indexAction()
+    {
+        $b = 4 + 5.001;
+        return new ViewModel();
+    }
+}
